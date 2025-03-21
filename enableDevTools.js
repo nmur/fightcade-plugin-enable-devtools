40ffe6e9e13f3484f6b10ce4b946bbbef0e0e9d0
Nicholas Murray <nickmurray91@gmail.com>
2025-03-22 02:28:26 +1100

    Update enableDevTools.js

--- enableDevTools.js
+++ enableDevTools.js
@@ -1,8 +1,10 @@
-module.exports = (FCADE) => {
+module.exports = (FCADE) => { runPlugin(FCADE) };
+
+const runPlugin = (FCADE) => {
     // Plugin code goes here
     document.addEventListener("keydown", (e) => {
         if (e.key === "F12") {
             require("electron").remote.getCurrentWindow().webContents.openDevTools();
         }
     });
-};
+}
